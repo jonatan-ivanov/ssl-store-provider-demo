e70cd52aa350e9fe66cb8c94af0fee287d4ea9a5
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2017-12-20 17:37:27 -0800

    Adding initial spring-boot application
    Empty app generated by https://start.spring.io/

---
+++ src/main/java/com/example/sslstoreproviderdemo/SslStoreProviderDemoApplication.java
@@ -0,0 +1,11 @@
+package com.example.sslstoreproviderdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslStoreProviderDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SslStoreProviderDemoApplication.class, args);
+	}
+}
